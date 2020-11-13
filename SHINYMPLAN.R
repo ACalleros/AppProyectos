4ef837f69b08b7b8b23828fc6fecb31cf70d3d24
ACalleros <jadriangc@gmail.com>
2020-11-12 18:00:23 -0600

    1

--- SHINYMPLAN.R
+++ SHINYMPLAN.R
@@ -9,41 +9,32 @@ distritos <- st_read(con, SQL('poligonos.pmdu_distritos')) %>%
   st_zm(drop = T)
 centros <- st_centroid(distritos)
 
+
 ui <- fluidPage(
-  ui <- fluidPage(
     titlePanel("Proyectos Estratégicos"),
-    sidebarLayout(
-      selectInput("variable",
-                  "Proyecto:",
-                  unique(distritos$estrategia)))),
-      mainPanel(leafletOutput('mymap'),
-                dataTableOutput('tabla'))
-    )
-  )
+      sidebarPanel(
+        selectInput("variable",
+                    "Proyecto:",
+                    unique(distritos$estrategia))),
+    mainPanel(leafletOutput('mymap'),
+              dataTableOutput('tabla'))
+    ))
 
 # server()
 server <- function(input, output){
   #MAPA ----
-  output$mymap <- renderLeaflet({ if (input$variable == "Gestión urbana") {
+  output$mymap <- renderLeaflet({ 
     leaflet() %>% 
       addProviderTiles("OpenStreetMap.Mapnik") %>% 
       addPolygons(data = distritos, fillOpacity = 0) %>% 
       addLabelOnlyMarkers(data = centros, label = 'oli') %>% 
       fitBounds(-101.33959, 19.61621, -101.08393, 19.77006 ) %>% 
       addPopups(data = centros, popup =distritos )
-  }
-    else {
-      leaflet() %>% 
-        addProviderTiles("OpenStreetMap.Mapnik") %>% 
-        addPolygons(data = distritos, fillOpacity = 0) %>% 
-        addPopups(data = centros, popup =distritos )
-  }
     })
   #TABLA ----
   output$tabla <- renderDataTable(distritos %>% 
     st_drop_geometry() %>% 
-    as.data.frame()
-  )
+    as.data.frame())
 }
   
 
