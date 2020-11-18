5db028aaae075d764750f92626ddfd5fc0485c8a
ACalleros <jadriangc@gmail.com>
2020-11-18 13:57:00 -0600

    3

--- Ejemplo.R
+++ Ejemplo.R
@@ -4,85 +4,71 @@ library(ggplot2)
 library(plotly)
 library(leaflet)
 library(DT)
-##############################################################################
-# Data
-##############################################################################
+
 qDat <- distritos
 qDat$id <- seq.int(nrow(qDat))
 str(qDat)
-##############################################################################
-# UI Side
-##############################################################################
+
 ui <- fluidPage(
-  titlePanel("Visualization of Fiji Earthquake"),
-  
-  # side panel
+  titlePanel("Programa Municipal de Desarrollo"),
   sidebarPanel(
-    h3('Fiji Earthquake Data'),
+    h4('Proyectos Estratégicos'),
     selectInput("subsistema",
-                "subsistema:",
-                unique(c(proyectos$SUBSISTEMA)), 
-                selected = 'Sistema Municipal de Áreas de Valor Ambiental (SMAVA)')),
-  
-  # main panel
+                "Subsistema:",
+                unique(c('Todos', c(proyectos$SUBSISTEMA))), 
+                selected = 'Todos')),
   mainPanel(
     leafletOutput('map01'),
-    dataTableOutput('table01')
+    dataTableOutput('table01'))
   )
-  
-)
-##############################################################################
-# Server Side
-##############################################################################
+
 server <- function(input,output){
-  qSub <-  reactive({
-    
-      subset <- subset(qDat %>% 
-                         st_drop_geometry() %>% 
-                         as.data.frame())
-                       })
+  qSub <- st_drop_geometry(qDat)
     # table
   output$table01 <- renderDataTable({
-    
-    DT::datatable(qDat, selection = "single",options=list(stateSave = TRUE))
+    DT::datatable(qSub, selection = "single",options=list(stateSave = TRUE))
   })
   
   # to keep track of previously selected row
   prev_row <- reactiveVal()
   
   observeEvent(input$table01_rows_selected, {
-    row_selected = qSub()[input$table01_rows_selected,]
+    row_selected = qSub[input$table01_rows_selected,]
     proxy <- leafletProxy('map01')
-    print(row_selected)
+    #print(row_selected)
     proxy %>%
       addPolygons(data = distritos[input$table01_rows_selected,], 
                   color = 'red')
     
-    # Reset previously selected marker
     if(!is.null(prev_row()))
     {
+      
       proxy %>%
-        addPolygons(data = st_as_sf(as.data.frame(prev_row)))
+        addMarkers(popup=as.character(prev_row()$mag), 
+                   layerId = as.character(prev_row()$id),
+                   lng=prev_row()$long, 
+                   lat=prev_row()$lat)
     }
-    prev_row(NULL)
+    
+   
   })
   
   # map
   output$map01 <- renderLeaflet({
       qMap <- leaflet() %>% 
-      addTiles() %>%
-     addPolygons(data = distritos)
+        addTiles() %>%
+        addPolygons(data = distritos,
+                    layerId = ~id)
     qMap
   })
   
   observeEvent(input$map01_shape_click, {
+    print(input$table01)
+    #print(input$map01_shape_click)
     clickId <- input$map01_shape_click$id
-    print(clickId)
-    dataTableProxy("table01") %>%
-      selectRows(which(qDat %>% 
-                         st_drop_geometry() %>% 
-                         as.data.frame(), View$id == clickId)) %>%
-      selectPage(which(input$table01_rows_all == clickId) %/% input$table01_state$length + 1)
+    dataTableProxy("table01") %>% 
+      selectRows(qSub[which(qSub$id_dto.x == clickId),]) 
+      #selectPage(qSub[which(input$table01_rows_all == clickId),] %/% input$table01_state$length + 1)
   })
 }
 
